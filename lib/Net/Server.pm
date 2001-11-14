5ca92510d5782edcf24af803523fa07aeed9dc0a
hookbot <hookbot>
2001-11-14 16:47:29 +0000

    Version 0.80 Actually pass the input reference for the mux_eof callback.

--- lib/Net/Server.pm
+++ lib/Net/Server.pm
@@ -34,7 +34,7 @@ use Net::Server::Daemonize qw(check_pid_file create_pid_file
                               safe_fork
                               );
 
-$VERSION = '0.79';
+$VERSION = '0.80';
 
 ### program flow
 sub run {
