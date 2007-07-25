13589e795b96694ffdb2a55cf8ff01311d795e91
rhandom <rhandom>
2007-07-25 15:44:03 +0000

    update

--- examples/connection_test.pl
+++ examples/connection_test.pl
@@ -1,10 +1,38 @@
 #!/usr/bin/perl -w
 
+=head1 NAME
+
+connection_test.pl - Test UDP/TCP/UNIX/UNIX_DGRAM connections
+
+=head1 SERVER SYNOPSIS
+
+    # in a separate terminal window
+
+    perl connection_test.pl
+
+=head1 CLIENT SYNOPSIS
+
+    perl connection_test.pl UDP
+
+    # or
+
+    perl connection_test.pl TCP
+
+    # or
+
+    perl connection_test.pl UNIX
+
+    # or
+
+    perl connection_test.pl UNIX_DGRAM
+
+=cut
+
 package MyPack;
 
 use strict;
-use vars qw(@ISA);
-use Net::Server ();
+use warnings;
+use base qw(Net::Server);
 use IO::Socket ();
 use POSIX qw(tmpnam);
 use Socket qw(SOCK_DGRAM SOCK_STREAM);
@@ -23,10 +51,10 @@ my $udp_port    = 20204;
 my $tcp_port    = 20204;
 
 print "\$Net::Server::VERSION = $Net::Server::VERSION\n";
-@ISA = qw(Net::Server);
 
 if( @ARGV ){
   if( uc($ARGV[0]) eq 'UDP' ){
+      print "Testing UDP\n";
     my $sock = IO::Socket::INET->new(PeerAddr => 'localhost',
                                      PeerPort => $udp_port,
                                      Proto    => 'udp',
@@ -34,12 +62,13 @@ if( @ARGV ){
     ### send a packet, get a packet
     $sock->send("Are you there?",0);
     my $data = undef;
-    $sock->recv($data,4096,0);  
+    $sock->recv($data,4096,0);
     print $data,"\n";
     exit;
   }
 
   if( uc($ARGV[0]) eq 'TCP' ){
+      print "Testing TCP\n";
     my $sock = IO::Socket::INET->new(PeerAddr => 'localhost',
                                      PeerPort => $tcp_port,
                                      Proto    => 'tcp',
@@ -51,6 +80,7 @@ if( @ARGV ){
   }
 
   if( uc($ARGV[0]) eq 'UNIX' ){
+      print "Testing UNIX (File socket with SOCK_STREAM)\n";
     my $sock = IO::Socket::UNIX->new(Peer => $socket_file) || die "Can't connect [$!]";
 
     print $sock "hi\n";
@@ -60,6 +90,7 @@ if( @ARGV ){
   }
 
   if( uc($ARGV[0]) eq 'UNIX_DGRAM' ){
+      print "Testing UNIX_DGRAM\n";
     my $sock = IO::Socket::UNIX->new(Peer => $socket_file2,
                                      Type => SOCK_DGRAM,
                                      ) || die "Can't connect [$!]";
@@ -70,7 +101,7 @@ if( @ARGV ){
     ### however, the default arguments don't seem to work for
     ### sending it back.  If anybody knows why, let me know.
     my $data = undef;
-    $sock->recv($data,4096,0);  
+    $sock->recv($data,4096,0);
     print $data,"\n";
     exit;
   }
