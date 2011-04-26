1b0ac04c155be329f6bfd3e315dbf8064af65883
rhandom <rhandom>
2011-04-26 15:12:23 +0000

    updates to test various variables, move to 4 space indent

--- examples/LoadTester.pl
+++ examples/LoadTester.pl
@@ -20,16 +20,16 @@ LoadTester.pl - Allow for testing load agains various servers
 
 =cut
 
-BEGIN {
-    Time::HiRes->import('time') if eval { require Time::HiRes };
-}
 use strict;
 use warnings;
 use base qw(Net::Server::PreFork);
 use IO::Socket;
+BEGIN {
+    Time::HiRes->import('time') if eval { require Time::HiRes };
+}
 
 $| = 1;
-__PACKAGE__->run(min_servers => 100, max_servers => 255);
+__PACKAGE__->run(min_servers => 100, max_servers => 255, max_spare_servers => 101);
 exit;
 
 ###----------------------------------------------------------------###
@@ -37,21 +37,24 @@ exit;
 ### set up the test parameters
 sub configure_hook {
     my $self = shift;
-    $self->{addr}       = 'localhost';   # choose a remote addr
-    $self->{port}       = 20203;         # choose a remote port
-    $self->{file}       = '/tmp/mysock'; # sock file for Load testing a unix socket
-    $self->{failed}     = 0;             # failed hits (server was blocked)
-    $self->{hits}       = 0;             # log hits
-    $self->{max_hits}   = 1000;          # how many impressions to do
-    $self->{time_begin} = time;          # keep track of time
-    $self->{sleep}      = 0;             # sleep between hits?
-    $self->{ssl}        = 0;             # use SSL ?
+    $self->{'addr'}        = 'localhost';   # choose a remote addr
+    $self->{'port'}        = 20203;         # choose a remote port
+    $self->{'file'}        = '/tmp/mysock'; # sock file for Load testing a unix socket
+    $self->{'failed'}      = 0;             # failed hits (server was blocked)
+    $self->{'hits'}        = 0;             # log hits
+    $self->{'hits2'}       = 0;             # log hits
+    $self->{'report_hits'} = 1000;          # how many hits in between reports
+    $self->{'max_hits'}    = 20_000;        # how many impressions to do
+    $self->{'time_begin'}  = time;          # keep track of time
+    $self->{'time_begin2'} = time;          # keep track of time
+    $self->{'sleep'}       = 0;             # sleep between hits?
+    $self->{'ssl'}         = 0;             # use SSL ?
 }
 
 
 ### these generally deal with sockets - ignore them
-sub pre_bind { require IO::Socket::SSL if shift->{ssl} }
-sub bind { shift()->log(2,"Running under pid $$\n") }
+sub pre_bind { require IO::Socket::SSL if shift->{'ssl'} }
+sub bind { shift()->log(2, "Running under pid $$") }
 sub accept { 1 }
 sub post_accept {}
 sub get_client_info {}
@@ -59,74 +62,72 @@ sub allow_deny { 1 }
 sub post_process_request {}
 
 
-### connect to the remote server and get some information
 sub process_request {
-  my $self = shift;
-  sleep($self->{sleep}) if $self->{sleep};
+    my $self = shift;
+    sleep $self->{'sleep'} if $self->{'sleep'};
 
-  ### try to connect and deliver the load
-  my $class = $self->{ssl} ? 'IO::Socket::SSL' : 'IO::Socket::INET';
-  if( $self->{remote} = $class->new(PeerAddr => $self->{addr},
-                                    PeerPort => $self->{port},
-                                    ) ){
-    $self->load;
-    return;
+    ### try to connect and deliver the load
+    my $class = $self->{'ssl'} ? 'IO::Socket::SSL' : 'IO::Socket::INET';
+    if ($self->{'remote'} = $class->new(PeerAddr => $self->{'addr'}, PeerPort => $self->{'port'})) {
+        $self->load;
+        return;
+    }
 
-  }
+    #if ($self->{remote} = IO::Socket::UNIX->new(Peer => $self->{'file'})) {
+    #  $self->load;
+    #  return;
+    #}
 
-  #if ($self->{remote} = IO::Socket::UNIX->new(Peer => $self->{file})) {
-  #  $self->load;
-  #  return;
-  #}
-
-  ### couldn't connect
-  *_WRITE = $self->{server}->{_WRITE};
-  _WRITE->autoflush(1);
-  print _WRITE "failed [$!]\n";
+    print { $self->{'server'}->{'_WRITE'} } "$$ failed [$!]\n";
 }
 
 
-### this is the test of the remote server
 sub load {
-  my $self = shift;
-  my $handle = $self->{remote};
-  $handle->autoflush(1);
-  my $line = <$handle>;
-  print $handle "quit\n";
+    my $self = shift;
+    my $handle = $self->{'remote'};
+    $handle->autoflush(1);
+    my $line = <$handle>;
+    print $handle "quit\n";
 }
 
 
-### keep track of what is going on
 sub parent_read_hook {
-  my $self   = shift;
-  my $status = shift;
+    my ($self, $status) = @_;
 
-  if( $status =~ /failed/i ){
-    $self->{failed}++;
-    print $status;
-    $self->{hits}--;
-  }
-  return '' unless   $status =~/processing/i;
-  print "*" unless $self->{hits} % 100;
-  return '' unless ++$self->{hits} >= $self->{max_hits};
+    if ($status =~ /failed/i) {
+        $self->{'failed'}++;
+        print $status;
+        if ($self->{'failed'} >= 300) {
+            $self->{'time_end'} = time;
+            $self->print_report;
+            $self->server_close;
+        }
+        return 1;
+    }
+    return if $status !~ /processing/i;
 
-  $self->{time_end} = time;
-  $self->print_report();
+    $self->{'hits'}++;
+    $self->{'hits2'}++;
+    print "*" if not $self->{'hits'} % 100;
+    if (not $self->{'hits'} % $self->{'report_hits'}) {
+        $self->{'time_end'} = time;
+        $self->print_report;
+        $self->{'hits2'} = 0;
+        $self->{'time_begin2'} = time;
+    }
 
-  return 1;
+    $self->server_close if $self->{'hits'} >= $self->{'max_hits'};
 }
 
 
-### spit out what happened
 sub print_report {
-  my $self = shift;
-  my $time = $self->{time_end} - $self->{time_begin};
+    my $self = shift;
+    my $time  = $self->{'time_end'} - $self->{'time_begin'};
+    my $time2 = $self->{'time_end'} - $self->{'time_begin2'};
 
-#  require "Data/Dumper.pm";
-#  print STDERR Data::Dumper::Dumper( $self );
-  print "\n$0 Results\n";
-  print "--------------------------------------------\n";
-  print "($self->{hits}) hits in ($time) seconds\n";
-  print "($self->{failed}) failed hits\n";
-  printf "%.3f hits per second\n", ($time ? $self->{hits}/$time : $self->{hits});
+    print "\n$0 Results\n";
+    print "--------------------------------------------\n";
+    printf "(%d) overall hits in (%.3f) seconds: %.3f hits per second\n", $self->{'hits'}, $time, ($time ? $self->{'hits'}/$time : $self->{'hits'});
+    printf "(%d) hits in (%.3f) seconds: %.3f hits per second\n", $self->{'hits2'}, $time2, ($time2 ? $self->{'hits2'}/$time2 : $self->{'hits2'});
+    print "($self->{failed}) failed hits\n";
 }
