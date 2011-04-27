cac790455623fb0b84ab8c4bb44a15b71b6c21cc
rhandom <rhandom>
2011-04-27 20:14:56 +0000

    code cleanup

--- lib/Net/Server/Daemonize.pm
+++ lib/Net/Server/Daemonize.pm
@@ -4,7 +4,7 @@
 #
 #  $Id$
 #
-#  Copyright (C) 2001-2007
+#  Copyright (C) 2001-2011
 #
 #    Jeremy Howard
 #    j+daemonize@howard.fm
@@ -25,185 +25,148 @@
 package Net::Server::Daemonize;
 
 use strict;
-use vars qw( @EXPORT_OK $VERSION );
 use base qw(Exporter);
 use POSIX qw(SIGINT SIG_BLOCK SIG_UNBLOCK);
 
-$VERSION = "0.05";
+our $VERSION = "0.06";
 
-@EXPORT_OK = qw(check_pid_file
-                create_pid_file
-                unlink_pid_file
-                is_root_user
-                get_uid get_gid
-                set_uid set_gid
-                set_user
-                safe_fork
-                daemonize
-                );
+our @EXPORT_OK = qw(check_pid_file    create_pid_file   unlink_pid_file
+                    is_root_user      get_uid get_gid   set_uid set_gid
+                    set_user          safe_fork         daemonize);
 
 ###----------------------------------------------------------------###
 
 ### check for existance of pid_file
 ### if the file exists, check for a running process
 sub check_pid_file ($) {
-  my $pid_file = shift;
+    my $pid_file = shift;
+    return 1 if ! -e $pid_file;
 
-  ### no pid_file = return success
-  return 1 unless -e $pid_file;
+    open my $fh, '<', $pid_file or die "Couldn't open existant pid_file \"$pid_file\" [$!]\n";
+    my $current_pid = <$fh>;
+    close $fh;
+    $current_pid = ($current_pid =~ /^(\d{1,10})/) ? $1 : die "Couldn't find pid in existing pid_file";
 
-  ### get the currently listed pid
-  if( ! open(_PID,$pid_file) ){
-    die "Couldn't open existant pid_file \"$pid_file\" [$!]\n";
-  }
-  my $_current_pid = <_PID>;
-  close _PID;
-  my $current_pid = $_current_pid =~ /^(\d{1,10})/ ? $1 : die "Couldn't find pid in existing pid_file";
+    my $exists;
+    if ($$ == $current_pid) {
+        warn "Pid_file created by this same process. Doing nothing.\n";
+        return 1;
+    } elsif (-d "/proc/$$") { # try a proc file system
+        $exists = -e "/proc/$current_pid";
+    } elsif (kill 0, $current_pid) {
+        $exists = 1;
+    }
+    die "Pid_file already exists for running process ($current_pid)... aborting\n"
+        if $exists;
 
-  my $exists;
-  if ($$ == $current_pid) {
-    warn "Pid_file created by this same process. Doing nothing.\n";
+    # remove the pid_file
+    warn "Pid_file \"$pid_file\" already exists.  Overwriting!\n";
+    unlink $pid_file || die "Couldn't remove pid_file \"$pid_file\" [$!]\n";
     return 1;
-  } elsif (-d "/proc/$$") { # try a proc file system
-    $exists = -e "/proc/$current_pid";
-  } elsif (kill 0, $current_pid) {
-    $exists = 1;
-  }
-  die "Pid_file already exists for running process ($current_pid)... aborting\n"
-    if $exists;
-
-  # remove the pid_file
-  warn "Pid_file \"$pid_file\" already exists.  Overwriting!\n";
-  unlink $pid_file || die "Couldn't remove pid_file \"$pid_file\" [$!]\n";
-  return 1;
 }
 
 ### actually create the pid_file, calls check_pid_file
 ### before proceeding
 sub create_pid_file ($) {
-  my $pid_file = shift;
+    my $pid_file = shift;
 
-  ### see if the pid_file is already there
-  check_pid_file( $pid_file );
+    check_pid_file($pid_file);
 
-  if( ! open(_PID, ">$pid_file") ){
-    die "Couldn't open pid file \"$pid_file\" [$!].\n";
-  }
+    open my $fh, '>', $pid_file or die "Couldn't open pid file \"$pid_file\" [$!].\n";
+    print $fh "$$\n";
+    close $fh;
 
-  ### save out the pid and exit
-  print _PID "$$\n";
-  close _PID;
-
-  die "Pid_file \"$pid_file\" not created.\n" unless -e $pid_file;
-  return 1;
+    die "Pid_file \"$pid_file\" not created.\n" if ! -e $pid_file;
+    return 1;
 }
 
 ### Allow for safe removal of the pid_file.
 ### Make sure this process owns it.
 sub unlink_pid_file ($) {
-  my $pid_file = shift;
+    my $pid_file = shift;
+    return 1 if ! -e $pid_file; # no pid_file = return success
 
-  ### no pid_file = return success
-  return 1 unless -e $pid_file;
+    open my $fh, '<', $pid_file or die "Couldn't open existant pid_file \"$pid_file\" [$!]\n"; # slight race
+    my $current_pid = <$fh>;
+    close $fh;
+    chomp $current_pid;
 
-  ### get the currently listed pid
-  if( ! open(_PID,$pid_file) ){
-    die "Couldn't open existant pid_file \"$pid_file\" [$!]\n";
-  }
-  my $current_pid = <_PID>;
-  close _PID;
-  chomp($current_pid);
+    die "Process $$ doesn't own pid_file \"$pid_file\". Can't remove it.\n"
+        if $current_pid ne $$;
 
-
-  if( $current_pid == $$ ){
     unlink($pid_file) || die "Couldn't unlink pid_file \"$pid_file\" [$!]\n";
     return 1;
-
-  }else{
-    die "Process $$ doesn't own pid_file \"$pid_file\". Can't remove it.\n";
-
-  }
-
 }
 
 ###----------------------------------------------------------------###
 
 sub is_root_user () {
-  my $id = get_uid('root');
-  return ( ! defined($id) || $< == $id || $> == $id );
+    my $id = get_uid('root');
+    return ! defined($id) || $< == $id || $> == $id;
 }
 
 ### get the uid for the passed user
 sub get_uid ($) {
-  my $user = shift;
-  my $uid  = undef;
-
-  if( $user =~ /^\d+$/ ){
-    $uid = $user;
-  }else{
-    $uid = getpwnam($user);
-  }
-
-  die "No such user \"$user\"\n" unless defined $uid;
-
-  return $uid;
+    my $user = shift;
+    my $uid  = ($user =~ /^(\d+)$/) ? $1 : getpwnam($user);
+    die "No such user \"$user\"\n" unless defined $uid;
+    return $uid;
 }
 
 ### get all of the gids that this group is (space delimited)
 sub get_gid {
-  my @gid  = ();
+    my @gid;
 
-  foreach my $group ( split( /[, ]+/, join(" ",@_) ) ){
-    if( $group =~ /^\d+$/ ){
-      push @gid, $group;
-    }else{
-      my $id = getgrnam($group);
-      die "No such group \"$group\"\n" unless defined $id;
-      push @gid, $id;
+    foreach my $group ( split( /[, ]+/, join(" ",@_) ) ){
+        if( $group =~ /^\d+$/ ){
+            push @gid, $group;
+        }else{
+            my $id = getgrnam($group);
+            die "No such group \"$group\"\n" unless defined $id;
+            push @gid, $id;
+        }
     }
-  }
 
-  die "No group found in arguments.\n" unless @gid;
-
-  return join(" ",$gid[0],@gid);
+    die "No group found in arguments.\n" unless @gid;
+    return join(" ",$gid[0],@gid);
 }
 
 ### change the process to run as this uid
 sub set_uid {
-  my $uid = get_uid( shift() );
+    my $uid = get_uid(shift());
 
-  POSIX::setuid($uid);
-  if ($< != $uid || $> != $uid) { # check $> also (rt #21262)
-    $< = $> = $uid; # try again - needed by some 5.8.0 linux systems (rt #13450)
-    if ($< != $uid) {
-      die "Couldn't become uid \"$uid\": $!\n";
+    POSIX::setuid($uid);
+    if ($< != $uid || $> != $uid) { # check $> also (rt #21262)
+        $< = $> = $uid; # try again - needed by some 5.8.0 linux systems (rt #13450)
+        if ($< != $uid) {
+            die "Couldn't become uid \"$uid\": $!\n";
+        }
     }
-  }
 
-  return 1;
+    return 1;
 }
 
 ### change the process to run as this gid(s)
 ### multiple groups must be space or comma delimited
 sub set_gid {
-  my $gids = get_gid( @_ );
-  my $gid  = (split /\s+/, $gids)[0];
-  eval { $) = $gids }; # store all the gids - this is really sort of optional
+    my $gids = get_gid(@_);
+    my $gid  = (split /\s+/, $gids)[0];
+    eval { $) = $gids }; # store all the gids - this is really sort of optional
 
-  POSIX::setgid($gid);
-  if (! grep {$gid == $_} split /\s+/, $() { # look for any valid id in the list
-    die "Couldn't become gid \"$gid\": $!\n";
-  }
+    POSIX::setgid($gid);
+    if (! grep {$gid == $_} split /\s+/, $() { # look for any valid id in the list
+        die "Couldn't become gid \"$gid\": $!\n";
+    }
 
-  return 1;
+    return 1;
 }
 
 ### backward compatibility sub
 sub set_user {
-  my ($user, @group) = @_;
-  set_gid( @group ) || return undef;
-  set_uid( $user )  || return undef;
-  return 1;
+    my ($user, @group) = @_;
+    set_gid(@group) || return undef;
+    set_uid($user)  || return undef;
+    return 1;
 }
 
 ###----------------------------------------------------------------###
@@ -211,93 +174,67 @@ sub set_user {
 ### routine to protect process during fork
 sub safe_fork () {
 
-  ### block signal for fork
-  my $sigset = POSIX::SigSet->new(SIGINT);
-  POSIX::sigprocmask(SIG_BLOCK, $sigset)
-    or die "Can't block SIGINT for fork: [$!]\n";
+    # block signal for fork
+    my $sigset = POSIX::SigSet->new(SIGINT);
+    POSIX::sigprocmask(SIG_BLOCK, $sigset) or die "Can't block SIGINT for fork: [$!]\n";
 
-  ### fork off a child
-  my $pid = fork;
-  unless( defined $pid ){
-    die "Couldn't fork: [$!]\n";
-  }
+    my $pid = fork;
+    die "Couldn't fork: [$!]" if ! defined $pid;
 
-  ### make SIGINT kill us as it did before
-  $SIG{INT} = 'DEFAULT';
+    $SIG{'INT'} = 'DEFAULT'; # make SIGINT kill us as it did before
 
-  ### put back to normal
-  POSIX::sigprocmask(SIG_UNBLOCK, $sigset)
-    or die "Can't unblock SIGINT for fork: [$!]\n";
+    POSIX::sigprocmask(SIG_UNBLOCK, $sigset) or die "Can't unblock SIGINT for fork: [$!]\n";
 
-  return $pid;
+    return $pid;
 }
 
 ###----------------------------------------------------------------###
 
-### routine to completely dissociate from
-### terminal process.
+### routine to completely dissociate from terminal process.
 sub daemonize ($$$) {
-  my ($user, $group, $pid_file) = @_;
+    my ($user, $group, $pid_file) = @_;
 
-  check_pid_file($pid_file) if defined $pid_file;
+    check_pid_file($pid_file) if defined $pid_file;
 
-  my $uid = get_uid( $user );
-  my $gid = get_gid( $group ); # returns list of groups
-  $gid = (split /\s+/, $gid)[0];
+    my $uid = get_uid($user);
+    my $gid = get_gid($group); # returns list of groups
+    $gid = (split /\s+/, $gid)[0];
 
+    my $pid = safe_fork();
 
-  my $pid = safe_fork();
+    exit(0) if $pid; # exit parent
 
-
-  ### parent process should do the pid file and exit
-  if( $pid ){
-
-    $pid && exit(0);
-
-
-  ### child process will continue on
-  }else{
-
+    # child
     create_pid_file($pid_file) if defined $pid_file;
-
-    ### make sure we can remove the file later
     chown($uid, $gid, $pid_file) if defined $pid_file;
 
-    ### become another user and group
     set_user($uid, $gid);
 
-    ### close all input/output and separate
-    ### from the parent process group
-    open STDIN,  '</dev/null' or die "Can't open STDIN from /dev/null: [$!]\n";
-    open STDOUT, '>/dev/null' or die "Can't open STDOUT to /dev/null: [$!]\n";
-    open STDERR, '>&STDOUT'   or die "Can't open STDERR to STDOUT: [$!]\n";
+    open STDIN,  '<', '/dev/null' or die "Can't open STDIN from /dev/null: [$!]\n";
+    open STDOUT, '>', '/dev/null' or die "Can't open STDOUT to /dev/null: [$!]\n";
+    open STDERR, '>&STDOUT'       or die "Can't open STDERR to STDOUT: [$!]\n";
 
-    ### Change to root dir to avoid locking a mounted file system
     ### does this mean to be chroot ?
-    chdir '/'                 or die "Can't chdir to \"/\": [$!]";
+    chdir '/' or die "Can't chdir to \"/\": [$!]";
 
-    ### Turn process into session leader, and ensure no controlling terminal
-    POSIX::setsid();
+    POSIX::setsid(); # Turn process into session leader, and ensure no controlling terminal
 
-    ### install a signal handler to make sure
-    ### SIGINT's remove our pid_file
-    $SIG{INT}  = sub { HUNTSMAN($pid_file) } if defined $pid_file;
+    ### install a signal handler to make sure SIGINT's remove our pid_file
+    $SIG{'INT'}  = sub { HUNTSMAN($pid_file) } if defined $pid_file;
     return 1;
-
-  }
 }
 
 ### SIGINT routine that will remove the pid_file
 sub HUNTSMAN {
-  my $path = shift;
-  unlink $path;
+    my $path = shift;
+    unlink $path;
 
-  eval {
-      require Unix::Syslog;
-      Unix::Syslog::syslog(Unix::Syslog::LOG_ERR(), "Exiting on INT signal.");
-  };
+    eval {
+        require Unix::Syslog;
+        Unix::Syslog::syslog(Unix::Syslog::LOG_ERR(), "Exiting on INT signal.");
+    };
 
-  exit;
+    exit;
 }
 
 
