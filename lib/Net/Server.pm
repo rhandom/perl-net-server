c636f018f31e03d06f51bfb9bb8ba9124252648d
rhandom <rhandom>
2012-06-08 21:24:40 +0000

    up the version

--- lib/Net/Server.pm
+++ lib/Net/Server.pm
@@ -32,7 +32,7 @@ use Net::Server::Proto ();
 use Net::Server::Daemonize qw(check_pid_file create_pid_file safe_fork
                               get_uid get_gid set_uid set_gid);
 
-our $VERSION = '2.004';
+our $VERSION = '2.005';
 
 sub new {
     my $class = shift || die "Missing class";
