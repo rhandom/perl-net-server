rhandom/perl-net-server

commit 5a689da0852d550ab301f0994b8a7820bba157a9

Add register_child hook (requested 6 years ago)

lib/Net/Server/Fork.pm
@@ -144,6 +144,7 @@ sub loop {
         # parent
         close($prop->{'client'}) if !$prop->{'udp_true'};
         $prop->{'children'}->{$pid}->{'status'} = 'processing';
+        $self->register_child($pid, 'fork');
     }
 }
 
@@ -164,7 +165,7 @@ sub accept {
     return undef if ! defined $sock;
 
     # check if this is UDP
-    if (SOCK_DGRAM == $sock->getsockopt(SOL_SOCKET,SO_TYPE)) {
+    if (SOCK_DGRAM == unpack('i', $sock->getsockopt(SOL_SOCKET, SO_TYPE))) {
         $prop->{'udp_true'} = 1;
         $prop->{'client'}   = $sock;
         $prop->{'udp_peer'} = $sock->recv($prop->{'udp_data'}, $sock->NS_recv_len, $sock->NS_recv_flags);

Makefile.PL
@@ -29,7 +29,7 @@ my %WriteMakefileArgs = (
     "Time::HiRes" => 0
   },
   "TEST_REQUIRES" => {},
-  "VERSION" => "2.010",
+  "VERSION" => "2.011",
   "test" => {
     "TESTS" => "t/*.t"
   }

lib/Net/Server.pm
@@ -707,6 +707,7 @@ sub done {
 }
 
 sub pre_fork_hook {}
+sub register_child {}
 sub child_init_hook {}
 sub child_finish_hook {}
 
@@ -730,6 +731,7 @@ sub run_dequeue { # fork off a child process to handle dequeuing
 
     $self->{'server'}->{'children'}->{$pid}->{'status'} = 'dequeue'
         if $self->{'server'}->{'children'};
+    $self->register_child($pid, 'dequeue');
 }
 
 sub default_port { 20203 }

lib/Net/Server.pod
@@ -743,7 +743,8 @@ For versions 2.000 through 2.004, the previous default of IPv4 was
 used.  We have attempted to make it easy to set IPv4, IPv6, or IPv*.
 If you do not want or need IPv6, simply set ipv to 4, pass IPv4 along
 in the port specification, set $ENV{'IPV'}=4; before running the
-server, or uninstall IO::Socket::IP and/or IO::Socket::INET6.
+server, set $ENV{'NO_IPV6'}, or uninstall IO::Socket::IP and/or
+IO::Socket::INET6.
 
 On my local box the following command results in the following output:
 
@@ -1508,6 +1509,12 @@ maintain the same random seed unless changed).
 =item C<$self-E<gt>pre_fork_hook()>
 
 Similar to the child_init_hook, but occurs just before the fork.
+
+=item C<$self-E<gt>register_child($pid, $type)>
+
+Called by parent process when a child has been forked.  Type
+will be one of dequeue, fork, prefork, or preforksimple depending
+on where the child was created.
 
 =item C<$self-E<gt>child_finish_hook()>
 

lib/Net/Server/PreFork.pm
@@ -174,7 +174,8 @@ sub run_n_children {
             }
 
             $prop->{'children'}->{$pid}->{'status'} = 'waiting';
-            $prop->{'tally'}->{'waiting'} ++;
+            $prop->{'tally'}->{'waiting'}++;
+            $self->register_child($pid, 'prefork');
 
         } else { # child
             if ($prop->{'child_communication'}) {

lib/Net/Server/PreForkSimple.pm
@@ -146,6 +146,7 @@ sub run_n_children {
 
         if ($pid) {
             $prop->{'children'}->{$pid}->{'status'} = 'processing';
+            $self->register_child($pid, 'preforksimple');
         } else {
             $self->run_child;
         }

commit f9219b43ceb2b973c5ea66767bac1a6415f07f8b

fix UDP recieving in Fork server
The return value of getsockopt should be used directly, not attempting
to convert with unpack. This matches how the other server classes do.

This bug was introduced in 5a689da0852d550ab301f0994b8a7820bba157a9
which was otherwise unrelated. The intent of the change in not obvious.

Fixes RT#146575

lib/Net/Server/Fork.pm
@@ -165,7 +165,7 @@ sub accept {
     return undef if ! defined $sock;
 
     # check if this is UDP
-    if (SOCK_DGRAM == unpack('i', $sock->getsockopt(SOL_SOCKET, SO_TYPE))) {
+    if (SOCK_DGRAM == $sock->getsockopt(SOL_SOCKET, SO_TYPE)) {
         $prop->{'udp_true'} = 1;
         $prop->{'client'}   = $sock;
         $prop->{'udp_peer'} = $sock->recv($prop->{'udp_data'}, $sock->NS_recv_len, $sock->NS_recv_flags);