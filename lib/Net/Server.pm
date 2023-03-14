fda98bac1a5fa63bd1f7b04762dafedb86cfad76
Paul Seamons <paul.seamons@newfold.com>
2023-03-14 11:13:23 -0600

    Update version

--- lib/Net/Server.pm
+++ lib/Net/Server.pm
@@ -29,7 +29,7 @@ use Net::Server::Proto ();
 use Net::Server::Daemonize qw(check_pid_file create_pid_file safe_fork
                               get_uid get_gid set_uid set_gid);
 
-our $VERSION = '2.013';
+our $VERSION = '2.014';
 
 sub new {
     my $class = shift || die "Missing class";
