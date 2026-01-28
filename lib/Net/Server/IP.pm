1e994ed5c538a136705a5e1aebb15d52d92fd3e5
Rob Brown <bbb@cpan.org>
2026-01-28 01:19:43 -0700

    Add Net::Server::IP pod

--- lib/Net/Server/IP.pm
+++ lib/Net/Server/IP.pm
@@ -1,3 +1,20 @@
+# -*- perl -*-
+#
+#  Net::Server::IP - IPv4 / IPv6 compatibility module
+#
+#  Copyright (C) 2025-2026
+#
+#    Rob Brown <bbb@cpan.org>
+#
+#  This package may be distributed under the terms of either the
+#  GNU General Public License
+#    or the
+#  Perl Artistic License
+#
+#  All rights reserved.
+#
+################################################################
+
 package Net::Server::IP;
 
 use strict;
@@ -26,3 +43,65 @@ sub configure {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Net::Server::IP - IPv4 / IPv6 compatibility module
+
+=head1 SYNOPSIS
+
+    use Net::Server::IP;
+
+    my $sock = Net::Server::IP->new(
+        LocalAddr => "[::]",
+        LocalPort => 8080,
+        Listen => 1,
+    ) or die "IPv6 listen error: $@ $!";
+
+    my $sock = Net::Server::IP->new(
+        PeerAddr => "[::1]:8080",
+    ) or die "IPv6 connect error: $@ $!";
+
+    my $sock = Net::Server::IP->new(
+        PeerAddr => "127.0.0.1:8080",
+    ) or die "IPv4 connect error: $@ $!";
+
+=head1 DESCRIPTION
+
+In order to support IPv6, Net::Server:IP inherits from either
+IO::Socket::IP or IO::Socket::INET6, whichever is available.
+This provides a consistent convention regardless of any
+differences between these modules. If only IPv4 is required
+based on its parameters, then neither module needs to be
+installed. Everything will still work fine by only inheriting
+from IO::Socket::INET. IO::Socket::IP will take preference,
+if available. To override this default behavior:
+
+    $Net::Server::IP::ipv6_package = 'Custom::Mod::Handle';
+
+=head1 INPUTS
+
+=head2 $sock = Net::Server::IP->new( %args )
+
+Creates a new C<Net::Server::IP> handle object.  The arguments
+recognized are similar to C<IO::Socket::IP> or C<IO::Socket::INET6>.
+
+=head1 AUTHOR
+
+Paul Seamons <paul@seamons.com>
+
+Rob Brown <bbb@cpan.org>
+
+=head1 SEE ALSO
+
+L<IO::Socket::IP>,
+L<IO::Socket::INET>,
+L<IO::Socket::INET6>
+
+=head1 LICENSE
+
+Distributed under the same terms as Net::Server
+
+=cut
